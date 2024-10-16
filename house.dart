1d19745a72fbb2782b37b1902e02a8e9e3954644
Ven <tivoenkong36@gmail.com>
2024-10-16 23:52:12 +0700

    OOP basic

---
+++ house.dart
@@ -0,0 +1,38 @@
+class Window{
+int quantity ; 
+String color ; 
+List<String> position=[] ; 
+
+Window(this.quantity , this.color , this.position)  ;
+
+
+
+}
+class Roof{
+String Type ;
+String Color ; 
+
+Roof(this.Type , this.Color) ; 
+
+
+
+}
+
+class Door{
+
+String Position ; 
+double width ;   
+ double height;
+
+
+Door(this.Position , this.width , this.height) ; 
+
+}
+class Chimney{
+
+double width ;   
+ double height;
+
+
+Chimney(this.width , this.height) ;
+}
